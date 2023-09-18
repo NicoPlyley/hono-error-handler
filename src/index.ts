7f0f4d32bc30918fab5baa5939810b4d10479674
Nico Plyley <nico@phoenixtechlab.com>
2023-09-18 14:37:45 -0700

    refactor: Moved types

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,17 @@
+import { Context } from 'hono';
+
 export { default as errorHandler } from './errorHandler';
 export { default as ErrorResponse } from './errorReponse';
 
-export interface CustomErrorHandler {
+export interface Error {
   statusCode?: number;
   stack?: string;
   message: string;
   [key: string]: any;
 }
+
+export interface FormattedError extends Error {
+  statusCode: number
+}
+
+export type ResponseHandler = (error: FormattedError, c: Context) => Response;
